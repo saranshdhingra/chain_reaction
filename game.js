a06279a33b3d02666f6350a7f3bbffb231e256b2
Saransh HP ac110tx <saranshdhingra92@gmail.com>
2017-09-17 20:54:14 +0530

    Working nicely, with 2 player mode. Some bugs present

--- game.js
+++ game.js
@@ -6,12 +6,15 @@ var tile_width=40,
 	game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update }),
 	tile_map,
 	clicked=false,
-	atoms=[];
+	animating=false,
+	atoms=[],
+	turn="red";
 
 function preload() {
 	// console.log("preload ran");
 	game.load.image('tile','images/tile.jpg');
-	game.load.image('ball', 'images/ball.png');
+	game.load.image('ball_red', 'images/ball_1.png');
+	game.load.image('ball_green', 'images/ball_2.png');
 }
 
 function create() {
@@ -21,23 +24,23 @@ function create() {
 		for(var j=0;j<num_cols;j++){
 			var data;
 			if(i==0 && j==0)
-				data={type:'corner',position:'left_top',balls:[]};
+				data={color:null,balls:[]};
 			else if(i==0 && j==num_cols-1)
-				data={type:'corner',position:'right_top',balls:[]};
+				data={color:null,balls:[]};
 			else if(i==num_rows-1 && j==0)
-				data={type:'corner',position:'left_bottom',balls:[]};
+				data={color:null,balls:[]};
 			else if(i==num_rows-1 && j==num_cols-1)
-				data={type:'corner',position:'right_bottom',balls:[]};
+				data={color:null,balls:[]};
 			else if(i==0)
-				data={type:'edge',position:'top',balls:[]}
+				data={color:null,balls:[]}
 			else if(i==num_rows-1)
-				data={type:'edge',position:'bottom',balls:[]};
+				data={color:null,balls:[]};
 			else if(j==0)
-				data={type:'edge',position:'left',balls:[]};
+				data={color:null,balls:[]};
 			else if(j==num_cols-1)
-				data={type:'edge',position:'right',balls:[]};
+				data={color:null,balls:[]};
 			else
-				data={type:'center',balls:[]};
+				data={color:null,balls:[]};
 			// atoms[i][j]=data;
 			row.push(data);
 		}
@@ -55,40 +58,91 @@ function update() {
 }
 
 function onClick(){
-	if(clicked)
+	if(clicked || animating){
+		// console.log("returning click",clicked,animating);
 		return;
+	}
 	clicked=true;
+	animating=true;
 
-	var x=Math.floor(game.input.activePointer.worldX/tile_width),
-		y=Math.floor(game.input.activePointer.worldY/tile_width);
-	
-	addAtom(x,y);
+	var col=Math.floor(game.input.activePointer.worldX/tile_width),
+		row=Math.floor(game.input.activePointer.worldY/tile_width),
+		atom=atoms[row][col];
+
+	// console.log(row,col);
+
+	//firstly don't let the player do anything if this atom has another color
+	if(atom.color!=null && atom.color!=turn){
+		animating=false;
+		return false;
+	}
+
+	addAtom(row,col,function(){
+		next_turn();
+		animating=false;
+	});
 }
 
-function addAtom(x,y,ball){
-	var atom=atoms[x][y],
-		neighbours=get_neighbours(x,y);
+function addAtom(row,col,onComplete){
+	var atom=atoms[row][col];
 
-	if(typeof ball=="undefined")
-		addBall(atom.balls,x,y);
-	else
-		atom.balls.push(ball);
+	atom.color=turn;
+	atom.balls.push(draw_atom(row,col));
+
 	//split the atom if number of balls has become equal to the number of neighbours
-	if(atom.balls.length == neighbours.length){
-		split_atoms(x,y,neighbours);
+	if(atom_needs_split(row,col)){
+		split_atoms(row,col,function(){
+			console.log("addAtom oncomplete being called");
+			onComplete();
+		});
 		// console.log(atom.balls.length,neighbours);
 	}
+	else{
+		align_balls(atom);
+		onComplete();
+	}
 }
 
-function split_atoms(x,y,neighbours){
-	var atom=atoms[x][y];
-	
-	for(var i=0;i<atom.balls.length;i++){
-		var ball=atom.balls[i],
-			neighbour=neighbours[i];
-		move_ball_to_neighbour(atom,neighbour,ball,i);
+function draw_atom(row,col,color){
+	var atom=atoms[row][col],
+		x=col*tile_width + tile_width/2,
+		y=row*tile_width + tile_width/2,
+		ball_str,
+		ball;
+
+	if(color===undefined){
+		ball_str=turn=="red"?'ball_red':'ball_green';
 	}
+	else
+		ball_str=color=="red"?"ball_red":"ball_green";
+
+	ball=game.add.sprite(x,y,ball_str);
+	ball.anchor.set(0.5);
+	return ball;
+}
+
+function split_atoms(row,col,onComplete){
+	console.log("split_atoms called for,",row,col);
+	var atom=atoms[row][col],
+		balls=atom.balls,
+		color=atom.color,
+		neighbours=get_neighbours(row,col);
+	
 	atom.balls=[];
+	atom.color=null;
+	for(var k=0;k<balls.length;k++){
+		var ball=balls[k],
+			neighbour=neighbours[k];
+
+		move_ball_to_neighbour(balls,color,neighbour,ball,k,function(index){
+			//this will run only after all balls have been moved to the neighbours
+			if(index==balls.length-1){
+				console.log("inner callback");
+				// console.log("split_atoms() running for",row,col);
+				onComplete();
+			}
+		});
+	}
 }
 
 function get_neighbours(x,y){
@@ -105,31 +159,87 @@ function get_neighbours(x,y){
 	return neighbours;
 }
 
-function addBall(balls,i,j){
-	var atom=atoms[i][j],
-		x=i*tile_width + tile_width/2,
-		y=j*tile_width + tile_width/2,
-		ball;
-
-	ball=game.add.sprite(x,y,'ball');
-	ball.anchor.set(0.5);
-	atom.balls.push(ball);
-}
-
-function move_ball_to_neighbour(atom,neighbour,ball,ball_index){
-	var i=neighbour[0],
-		j=neighbour[1],
-		x=i*tile_width+tile_width/2,
-		y=j*tile_width+tile_width/2,
-		neighbour_atom=atoms[i][j],
+function move_ball_to_neighbour(balls,color,neighbour,ball,ball_index,callback){
+	var row=neighbour[0],
+		col=neighbour[1],
+		x=col*tile_width+tile_width/2,
+		y=row*tile_width+tile_width/2,
+		neighbour_atom=atoms[row][col],
 		tween=game.add.tween(ball);
 
 	// console.log(neighbour_atom);
 
-	tween.to({x:x,y:y},1000,'Linear',true,0);
+	tween.to({x:x,y:y},600,'Linear',true,0);
 	tween.onComplete.add(function(){
-		// neighbour_atom.balls.push(ball);
-		addAtom(i,j,ball);
+		neighbour_atom.balls.push(ball);
+		// console.log(neighbour_atom.color,turn);
+		if(neighbour_atom.color!=null && neighbour_atom.color!=color){
+			console.log("changing color",row,col,color,neighbour_atom.color);
+			change_atom_color(row,col,color);
+		}
+		if(atom_needs_split(row,col)){
+			console.log("atom needs a split");
+			split_atoms(row,col,function(){
+				console.log("split complete");
+				callback(ball_index);
+			});
+		}
+		else{
+			align_balls(neighbour_atom);
+			callback(ball_index);
+		}
+		// else
+		// 	addAtom(i,j,ball);
 		// atom.balls.splice(ball_index,1);
 	},this);
+}
+
+function change_atom_color(row,col,color){
+	var atom=atoms[row][col],
+		num_atoms=atom.balls.length;
+
+	atom.color=color;
+	atom.balls.forEach(function(ball){
+		ball.destroy();
+	});
+	atom.balls=[];
+
+	for(var k=0;k<num_atoms;k++){
+		atom.balls.push(draw_atom(row,col,color))
+	}
+}
+
+//since all balls shouldn't be superimposed
+//this function moves the balls so that all are visible
+function align_balls(atom){
+	if(atom.balls.length==2){
+		atom.balls[0].x-=2;
+		atom.balls[0].y-=2;
+		
+		atom.balls[1].x+=2;
+		atom.balls[1].y+=2;
+	}
+	else if(atom.balls.length==3){
+		atom.balls[0].y-=4;
+		
+		atom.balls[1].x-=4;
+		atom.balls[1].y+=4;
+		
+		atom.balls[2].x+=4;
+		atom.balls[2].y+=4;	
+	}
+}
+
+function atom_needs_split(row,col){
+	var atom=atoms[row][col],
+		neighbours=get_neighbours(row,col);
+
+	if(atom.balls.length == neighbours.length)
+		return true;
+
+	return false;
+}
+
+function next_turn(){
+	turn=turn=="red"?"green":"red";
 }
